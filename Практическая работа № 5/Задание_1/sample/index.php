c8c1541409b6a270580803e73d62f8a09e3efa89
safiulinroman <safiulin-93@mail.ru>
2026-05-05 10:19:28 +0600

    5

---
+++ Практическая работа № 5/Задание_1/sample/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Программирование на языке PHP</title>
+</head>
+<body>
+	<h1>Функции</h1>
+	<h2>Встроенные функции, часть 1</h2>
+	
+	<?php
+		require "teams.php";
+
+		// ...
+	
+	?>
+	
+
+</body>
+</html>
---
+++ Практическая работа № 5/Задание_5/sample/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Программирование на языке PHP</title>
+</head>
+<body>
+	
+	<h1>Функции</h1>
+	<h2>Встроенные функции, часть 2</h2>
+	
+	<?php
+		// ?search=teams::3
+		// ?search=albums::5
+		// ?search=tracks::10
+		
+		// ..
+		
+	?>
+	
+
+</body>
+</html>
