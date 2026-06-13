42fa133297a6721f0788b3f658941be611425e6f
safiulinroman <safiulin-93@mail.ru>
2026-06-13 08:12:07 +0600

    16

---
+++ Практическая работа № 12/Задание_2/sample/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Программирование на языке PHP</title>
+</head>
+<body>
+	<h1>Отправка данных на сервер</h1>
+	<h2>Регулярные выражения, часть 1</h2>
+
+	<?php
+		
+		// ...
+		
+	?>
+
+
+</body>
+</html>
